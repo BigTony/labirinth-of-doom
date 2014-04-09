716b6cd4954323bb38bfe41dd52924b7da95b013
Antonin Seidl <tonyseidl.cz@gmail.com>
2014-04-09 23:14:55 +0200

    server1

--- src/server_connect.cpp
+++ src/server_connect.cpp
@@ -5,7 +5,51 @@
 // Skyva Petr     - xskyva02
 ///////////////////////////////////
 // server_connect.cpp
+#include <ctime>
+#include <iostream>
+#include <string>
+#include "server_connect.hpp"
 
-#define "server_connect.hpp"
+
+using boost::asio::ip::tcp;
 
 
+class connection_binnder {
+public:
+  
+ /**
+  * A constructor.
+  * Constructor of connection_binnder no parrams need.
+  */
+  connection_binnder(){   
+  acceptor(io_service, tcp::endpoint(tcp::v4(), SERVER_PORT));    
+  }
+  
+ /**
+  * A public function.
+  * Waiting for incomming connection from clients.
+  * When connection comes handle it and add it to @var connections
+  */  
+  wait_connection();
+private:
+  /**
+  * A private variable 
+  * Any program that uses asio need to have at least one io_service object
+  */  
+  boost::asio::io_service io_service;
+  
+  
+  /**
+  * A private variable 
+  * Acceptor object needs to be created to listen for new connections
+  */  
+  tcp::acceptor;
+  
+  /**
+  * A private variable 
+  * Array storing client connections
+  */   
+  client_connection * connections;
+  
+  
+}
--- src/server_connect.hpp
+++ src/server_connect.hpp
@@ -13,6 +13,72 @@
 
 
 
+#include <boost/asio.hpp>
+
+#define SERVER_PORT "11500"
+
+using boost::asio::ip::tcp;
+
+
+/**
+* Handling all incomming cnnections
+*/
+class connection_binnder {
+public:
+  
+ /**
+  * A constructor.
+  * Constructor of connection_binnder no parrams need.
+  */
+  connection_binnder();
+  
+ /**
+  * A public function.
+  * Waiting for incomming connection from clients.
+  * When connection comes handle it and add it to @var connections
+  */  
+  wait_connection();
+private:
+  
+ /**
+  * A public function.
+  * Waiting for incomming connection from clients.
+  * When connection comes handle it and add it to @var connections
+  */  
+  add_connection();  
+  
+  
+  /**
+  * A private variable 
+  * Any program that uses asio need to have at least one io_service object
+  */  
+  boost::asio::io_service io_service;
+  
+  /**
+  * A private variable 
+  * Acceptor object needs to be created to listen for new connections
+  */  
+  tcp::acceptor;
+  
+  /**
+  * A private variable 
+  * Array storing client connections
+  */   
+  client_connection * connections;
+  
+  
+}
+
+
+/**
+* Class storing all information about connected clients
+*/
+class client_connection {
+public:
+
+  client_connection() {
+  
+}
 
 /**
 * 
