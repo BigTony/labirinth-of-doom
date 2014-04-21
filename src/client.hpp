8afa8ab3c77577c25819002d3d8d2eec7f741450
Antonin Seidl <tonyseidl.cz@gmail.com>
2014-04-21 22:12:07 +0200

    WOOOOP

---
+++ src/client.hpp
@@ -0,0 +1,43 @@
+// Project labirinth of doom ICP2014
+// Dev team:
+////////////////////////////////////
+// Seidl Antonin  - xseidl01
+// Skyva Petr     - xskyva02
+///////////////////////////////////
+// client.hpp
+
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <boost/thread/thread.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class game_client{
+  public:
+  game_client(std::string server_ip);
+  void run();
+  void terminal_command();
+  
+  
+private:
+  boost::asio::io_service io_;
+  tcp::resolver resolver_;
+  tcp::resolver::iterator endpoint_;
+  boost::thread t_connection_;
+  server_connection connection_;
+  std::string command_;
+};
+
+
+
+
+
+
+
+
+#endif
