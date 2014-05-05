9ba5f11495421a5d6d6612936d2369a6964ce72a
JoystiC <petr.skyvaa@seznam.cz>
2014-05-05 16:15:18 +0200

    maze konstruktori + nacteni levlu

--- src/maze.cpp
+++ src/maze.cpp
@@ -7,9 +7,109 @@
 // maze.cpp
 
 #include "maze.hpp"
+#include "output.hpp"
+
+void maze::add_object(std::string value){
+	// std::cout << value << std::endl;
+	if(value == ""){
+		maze_array_.emplace_back(path_free());
+		// out_print_debug_object(" \t")
+	}else if(value == "w"){
+		maze_array_.emplace_back(wall_object());
+		// out_print_debug_object("w\t")
+	}else if(value == "f"){
+		maze_array_.emplace_back(finish_object());
+		// out_print_debug_object("f\t")
+	}else if(value.compare(0,2, "G_") == 0){
+		maze_array_.emplace_back(gate_object(value.substr(2)));
+		// out_print_debug_object("G\t")
+	}else if(value.compare(0,2, "S_") == 0){
+		maze_array_.emplace_back(keeper_object(value.substr(2)));
+		// out_print_debug_object("G\t")
+	}else if(value.compare(0,2, "K_") == 0){
+		maze_array_.emplace_back(key_object(value.substr(2)));
+		// out_print_debug_object("G\t")
+	}else if(value.compare(0,3, "Cp_") == 0){
+		maze_array_.emplace_back(create_player_object(value.substr(3)));
+		// out_print_debug_object("G\t")
+	}else{
+		// out_print_error("EERR");
+	}
+}
+
+maze::maze(){
+
+}
+
+maze_object::maze_object(){
+
+}
+
+static_object::static_object(){
+
+}
+
+dynamic_object::dynamic_object(){
+
+}
+
+key_object::key_object(std::string s_id){
+	id = std::stoi(s_id);
+}
+
+gate_object::gate_object(std::string s_id){
+	id = std::stoi(s_id);
+}
+
+wall_object::wall_object(){
+
+}
+
+path_free::path_free(){
+
+}
+
+player_object::player_object(){
+
+}
+
+keeper_object::keeper_object(std::string s_id){
+	id = std::stoi(s_id);
+}
+
+finish_object::finish_object(){
+
+}
+
+create_player_object::create_player_object(std::string s_id){
+	id = std::stoi(s_id);
+}
 
 
 
 
+int main(int argc, char* argv[]){
+	std::ifstream file ( "levels/level1.csv" ); // declare file stream: http://www.cplusplus.com/reference/iostream/ifstream/
+	std::string value = "";
+
+	maze maze;
+
+	while ( file.good() ){
+    	char c = file.get();
+
+		if (file.good()){
+			if(c == ','){
+				maze.add_object(value);
+				value = "";
+			}else{
+				value +=c;
+			}
+		}   		
+   	}
+
+   	file.close();
+
+   	return 0;
+}
 
     
