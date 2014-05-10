dadbc1c2bb832529615ad33485f6e8a10983b257
Antonin Seidl <tonyseidl.cz@gmail.com>
2014-05-10 19:55:07 +0200

    add main

---
+++ src/server_main.cpp
@@ -0,0 +1,32 @@
+// Project labirinth of doom ICP2014
+// Dev team:
+////////////////////////////////////
+// Seidl Antonin  - xseidl01
+// Skyva Petr     - xskyva02
+///////////////////////////////////
+// server.cpp
+#include <ctime>
+#include <iostream>
+#include <string>
+#include "server.hpp"
+#include "output.hpp"
+
+using boost::asio::ip::tcp;
+
+
+int main(int argc, char* argv[]){
+
+  try{
+	  std::string arg = "argv[1]";
+	  if (arg.compare("-debug")){
+	  }
+    game_server server;
+    server.run();
+    server.terminal_command();
+  }
+  catch (std::exception& error){
+    out.print_error(std::string("Exception: ")+error.what());
+  }
+
+  return 0;
+}
